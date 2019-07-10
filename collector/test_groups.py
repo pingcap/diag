a641d453a0a3a83b678fe1038411f73e3340a0a9
Shafreeck Sea <shafreeck@gmail.com>
2019-07-10 17:18:47 +0800

    collector: fix broken imports

--- collector/test_groups.py
+++ collector/test_groups.py
@@ -1,7 +1,7 @@
 # coding: utf8
 import unittest
 from groups import CollectorGroup
-from collector import Collector
+from collectors import Collector
 
 
 class TestCollectorGroup(unittest.TestCase):
