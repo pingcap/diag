97cc09032486cb6058b5bf194865ff625f451f0e
Shafreeck Sea <shafreeck@gmail.com>
2019-07-10 15:41:28 +0800

    collector: implement profile collector

--- collector/collectors/profile_collector.py
+++ collector/collectors/profile_collector.py
@@ -1,22 +1,101 @@
 from collector import Collector
 import urllib
+import urllib2
+import uuid
+import socket
 
 
 class HTTPCollector(Collector):
-    def collect(self, addr='127.0.0.1:8804', path='', opts={}):
-        url = "http://%s%s?%s" % (addr, path, urllib.urlencode(opts))
-        f = urllib.urlopen(url)
+    def __init__(self, name='http', addr='172.0.0.1:80',
+                 path='/', params={}):
+        self.id = uuid.uuid4()
+        self.name = name
+        self.addr = addr
+        self.path = path
+        self.params = params
+
+    def __repr__(self):
+        return "%s %s %s" % (self.id, self.name, self.url())
+
+    def url(self):
+        params = ""
+        if len(self.params) > 0:
+            params = "?" + urllib.urlencode(self.params)
+        url = "http://%s%s%s" % (self.addr, self.path, params)
+        return url
+
+    def collect(self):
+        f = urllib2.urlopen(self.url())
         return f.read()
 
 
-class CPUProfileCollector(HTTPCollector):
-    def __init__(self, name='profile'):
-        self.name = name
+class PProfHTTPCollector(HTTPCollector):
+    def __init__(self, name='pprof', addr='127.0.0.1:6060',
+                 path='/debug/pprof', params={}):
+        HTTPCollector.__init__(self, name, addr, path, params)
 
-    def collect(self, addr='127.0.0.1:8804', path='/debug/pprof/profile',
-                opts={'seconds': 10}):
+    def collect(self):
+        seconds = self.params.get('seconds')
+        if seconds != None:
+            timeout = float(seconds) + 10
+            socket.setdefaulttimeout(timeout)
+        f = urllib2.urlopen(self.url())
+        return f.read()
+
+
+class CPUProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='cpuprofile', addr='172.0.0.1:6060',
+                 path='/debug/pprof/profile', params={'seconds': 10}):
+        PProfHTTPCollector.__init__(self, name, addr, path, params)
+
+
+class MemProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='memprofile', addr='127.0.0.1:6060',
+                 path='/debug/pprof/heap'):
+        PProfHTTPCollector.__init__(self, name, addr, path)
+
+
+class BlockProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='blockprofile', addr='127.0.0.1:6060',
+                 path='/debug/pprof/block'):
+        PProfHTTPCollector.__init__(self, name, addr, path)
+
+
+class AllocsProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='allocsprofile', addr='127.0.0.1:6060',
+                 path='/debug/pprof/allocs'):
+        PProfHTTPCollector.__init__(self, name, addr, path)
+
+
+class GoroutineProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='goroutineprofile', addr='127.0.0.1:6060',
+                 path='/debug/pprof/goroutine'):
+        PProfHTTPCollector.__init__(self, name, addr, path)
+
+
+class MutexProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='mutexprofile', addr='127.0.0.1:6060',
+                 path='/debug/pprof/mutex'):
+        PProfHTTPCollector.__init__(self, name, addr, path)
+
+
+class TheadCreateProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='threadcreateprofile', addr='127.0.0.1:6060',
+                 path='/debug/pprof/threadcreate'):
+        PProfHTTPCollector.__init__(self, name, addr, path)
+
+
+class TraceProfileCollector(PProfHTTPCollector):
+    def __init__(self, name='traceprofile', addr='127.0.0.1:6060',
+                 path='/debug/pprof/trace', params={}):
+        PProfHTTPCollector.__init__(self, name, addr, path, params)
 
 
 if __name__ == "__main__":
-    c = CPUProfileCollector()
-    print c.collect('127.0.0.1:8000')
+    c = CPUProfileCollector(addr='127.0.0.1:8000')
+    print c
+    print c.collect()
+
+    c = MemProfileCollector(addr='127.0.0.1:8000')
+    print c
+    print c.collect()
