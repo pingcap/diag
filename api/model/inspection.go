eb8c76c5e5c1ea1c3d71c61ed373f97f8c392393
lucklove <gnu.crazier@gmail.com>
2019-07-26 17:36:47 +0800

    api: fix inspections pagination

--- api/model/inspection.go
+++ api/model/inspection.go
@@ -26,7 +26,10 @@ type Inspection struct {
 func (m *Model) ListAllInspections(page, size int64) ([]*Inspection, int, error) {
 	inspections := []*Inspection{}
 
-	rows, err := m.db.Query("SELECT id,instance,status,type,create_t,tidb,tikv,pd,grafana,prometheus FROM inspections")
+	rows, err := m.db.Query(
+		"SELECT id,instance,status,type,create_t,tidb,tikv,pd,grafana,prometheus FROM inspections limit ?,?",
+		(page-1)*size, size,
+	)
 	if err != nil {
 		log.Error("failed to call db.Query:", err)
 		return nil, 0, err
