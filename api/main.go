0d69dacf74cc2d982bbfbe764f74af435703ba9a
lucklove <gnu.crazier@gmail.com>
2019-07-29 11:32:57 +0800

    simplify command line

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"path"
 	"flag"
 
 	"github.com/pingcap/tidb-foresight/bootstrap"
@@ -11,9 +12,9 @@ import (
 func main() {
 	homepath := flag.String("home", "/tmp/tidb-foresight", "tidb-foresight work home")
 	address := flag.String("address", "0.0.0.0:8888", "tidb foresight listen address")
-	pioneer := flag.String("pioneer", "/tmp/pioneer.py", "tool to parse inventory.ini and get basic cluster info")
-	collector := flag.String("collector", "/tmp/pioneer.py", "tool to collect cluster info")
-	analyzer := flag.String("analyzer", "/tmp/pioneer.py", "tool to analyze cluster info")
+	pioneer := flag.String("pioneer", "", "tool to parse inventory.ini and get basic cluster info")
+	collector := flag.String("collector", "", "tool to collect cluster info")
+	analyzer := flag.String("analyzer", "", "tool to analyze cluster info")
 
 	flag.Parse()
 
@@ -29,6 +30,16 @@ func main() {
 	config.Pioneer = *pioneer
 	config.Collector = *collector
 	config.Analyzer = *analyzer
+
+	if config.Pioneer == "" {
+		config.Pioneer = path.Join(config.Home, "bin", "pionner")
+	}
+	if config.Collector == "" {
+		config.Collector = path.Join(config.Home, "bin", "collector")
+	}
+	if config.Analyzer == "" {
+		config.Analyzer = path.Join(config.Home, "bin", "analyzer")
+	}
 
 	s := server.NewServer(config, db)
 
