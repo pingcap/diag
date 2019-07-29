e06063c5c0faf1be6cfadde452d256608e515e8f
Shafreeck Sea <shafreeck@gmail.com>
2019-07-29 21:59:10 +0800

    collector: remove unused dependencies
    Signed-off-by: Shafreeck Sea <shafreeck@gmail.com>

--- collector/collectors/command_collector.py
+++ collector/collectors/command_collector.py
@@ -1,11 +1,6 @@
 # coding:utf8
 from collector import Collector
-import os
-import sys
-if os.name == 'posix' and sys.version_info[0] < 3:
-    import subprocess32 as subprocess
-else:
-    import subprocess
+import subprocess
 
 
 class CommandCollector(Collector):
