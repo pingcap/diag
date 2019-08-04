6d23bb4322330ac7133b9cbb3baa9b208cd7b098
lucklove <gnu.crazier@gmail.com>
2019-08-04 21:24:43 +0800

    bugfix: metric proxy will double the url path
    The way to get proxy will generate a url, which contains the path,
    however, the request path assign intruduce the path again.

--- api/server/metric.go
+++ api/server/metric.go
@@ -9,17 +9,14 @@ import (
 )
 
 func (s *Server) metric(w http.ResponseWriter, r *http.Request) {
-	u := "http://localhost:8888" + strings.Replace(r.URL.Path, "/metric", "", 1) + "?" + r.URL.RawQuery
+	u := "http://localhost:8080/api/v1"
 	url, err := url.Parse(u)
 	if err != nil {
-		log.Info("parse url: ", err)
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"status": "BAD_REQUEST", "message": "url invalid"}`))
+		log.Panic("parse url:", err)
 	}
 
-	r.URL.Scheme = url.Scheme
 	r.URL.Host = url.Host
-	r.URL.Path = url.Path
+	r.URL.Path = strings.Replace(r.URL.Path, "/api/v1/metric", "", 1)
 	r.Host = url.Host
 
 	proxy := httputil.NewSingleHostReverseProxy(url)
