73c6c4bac6aa24f66e71042e02ec525f2fa30310
Shafreeck Sea <shafreeck@gmail.com>
2019-08-06 18:49:43 +0800

    collector: work aroud bug of python2.7
    fix TF-43
    
    Signed-off-by: Shafreeck Sea <shafreeck@gmail.com>

--- collector/collectors/command_collector.py
+++ collector/collectors/command_collector.py
@@ -14,7 +14,12 @@ class CommandCollector(Collector):
         if self.addr:
             command = "ssh tidb@%s 'bash -c \"%s\"'" % (
                 self.addr, self.command)
-        return subprocess.check_output(command, shell=True)
+        # work aroud bug of python2.7
+        # https://bugs.python.org/issue9400
+        try:
+            return subprocess.check_output(command, shell=True)
+        except subprocess.CalledProcessError as cpe:
+            raise Exception(str(cpe))
 
 
 if __name__ == "__main__":
