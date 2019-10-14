c3d4f3fefe8b8a9900bae15f004adc109e9ae31d
mapleFU <1506118561@qq.com>
2019-10-14 20:03:47 +0800

    [Update] Adding config in main.go and supporting it in service template
    Signed-off-by: mapleFU <1506118561@qq.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,6 +14,9 @@ func main() {
 	homepath := flag.String("home", "/tmp/tidb-foresight", "tidb-foresight work home")
 	address := flag.String("address", "0.0.0.0:9527", "tidb foresight listen address")
 
+	prometheusAddress := flag.String("prometheus", "127.0.0.1:9529", "prometheus listen address")
+	influxdbAddress := flag.String("influxdb", "127.0.0.1:9528", "influxdb listen address")
+
 	flag.Parse()
 
 	log.SetFormatter(&log.TextFormatter{
@@ -26,6 +29,10 @@ func main() {
 	}
 
 	config, db := bootstrap.MustInit(*homepath)
+
+	config.Influx.Endpoint = *influxdbAddress
+	config.Prometheus.Endpoint = *prometheusAddress
+
 	defer db.Close()
 
 	s := server.New(config, db)
