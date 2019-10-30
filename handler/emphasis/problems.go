e056f2fea2d98001404c0dd0ba54fab7ce6d4c76
mapleFU <1506118561@qq.com>
2019-10-30 17:45:19 +0800

    [Update] Add api for symptom.
    Signed-off-by: mapleFU <1506118561@qq.com>

---
+++ handler/emphasis/problems.go
@@ -0,0 +1,40 @@
+package emphasis
+
+import (
+	"github.com/pingcap/tidb-foresight/wrapper/db"
+	"net/http"
+
+	"github.com/pingcap/fn"
+	helper "github.com/pingcap/tidb-foresight/handler/utils"
+	"github.com/pingcap/tidb-foresight/model"
+	"github.com/pingcap/tidb-foresight/model/emphasis"
+	"github.com/pingcap/tidb-foresight/utils"
+)
+
+type loadProblemsHandler struct {
+	m model.Model
+}
+
+func LoadAllProblems(m model.Model) http.Handler {
+	return &loadProblemsHandler{m}
+}
+
+func (h *loadProblemsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fn.Wrap(h.listAllProblems).ServeHTTP(w, r)
+}
+
+func (h *loadProblemsHandler) listAllProblems(r *http.Request) ([]*emphasis.Problem, utils.StatusError) {
+	uuid := helper.LoadRouterVar(r, "uuid")
+	emp, err := h.m.GetEmphasis(uuid)
+	if err != nil {
+		if db.IsNotFound(err) {
+			return nil, utils.TargetObjectNotFound
+		}
+		return nil, utils.DatabaseQueryError
+	}
+	problems, err := h.m.LoadAllProblems(emp)
+	if err != nil {
+		return nil, utils.DatabaseQueryError
+	}
+	return problems, nil
+}
