e45a63a36c86a837fe58d42664855964a4743183
mapleFU <1506118561@qq.com>
2019-10-31 16:08:08 +0800

    [Fix] Fix web deploying
    Signed-off-by: mapleFU <1506118561@qq.com>

--- scripts/install.py
+++ scripts/install.py
@@ -146,7 +146,7 @@ if __name__ == '__main__':
     if not os.path.exists(prefix):
         os.makedirs(prefix)
 
-    to_copy_directories = ['bin', 'web-dist', 'conf']
+    to_copy_directories = ['bin', 'conf']
     # 最终的 $prefix/tidb-foresight 文件
     dest_dir = os.path.join(prefix, 'tidb-foresight')
     # override
@@ -164,16 +164,14 @@ if __name__ == '__main__':
     # check tidb-foresight.toml
     # if exists, then copy it to $prefix/tidb-foresight
     if os.path.exists('tidb-foresight.toml'):
-        os.system(
-            "'cp' -f tidb-foresight.toml {}/tidb-foresight.toml".format(
-                dest_dir))
+        os.system("'cp' -f tidb-foresight.toml {}/tidb-foresight.toml".format(
+            dest_dir))
 
     for to_copy_directory in to_copy_directories:
         os.system("'cp' -rf {} {}".format(to_copy_directory, dest_dir))
     # rename web-dist to web
-    print("mv {dest_dir}/web-dist {dest_dir}/web".format(dest_dir=dest_dir))
     os.system(
-        "rm -rf {dest_dir}/web {dest_dir}/web-dist && 'cp' -rf web-dist {dest_dir}/ && mv {dest_dir}/web-dist {dest_dir}/web"
+        "rm -rf {dest_dir}/web {dest_dir}/web-dist && 'cp' -rf web/dist {dest_dir}/ && mv {dest_dir}/dist {dest_dir}/web"
         .format(dest_dir=dest_dir))
 
     os.system("'cp' -rf *.service {}".format(dest_dir))
