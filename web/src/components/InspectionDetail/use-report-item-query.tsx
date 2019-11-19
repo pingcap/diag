333258ef231207d8bbbcd65c733ca319089bc7e4
baurine <2008.hbl@gmail.com>
2019-11-19 17:32:26 +0800

    refactor - polish use-report-item-query

--- web/src/components/InspectionDetail/use-report-item-query.tsx
+++ web/src/components/InspectionDetail/use-report-item-query.tsx
@@ -47,96 +47,104 @@ export function useReportItemQuery(
     }
   }
 
+  function parseResAsArr(res: IResConclusionWithData) {
+    let containsAbnormal =
+      (res.conclusion as IConclusion[]).find(item => item.status === 'error') !== undefined;
+    const columns = Object.keys(res.data[0] || {}).map(key => ({
+      title: key,
+      dataIndex: key,
+      key,
+      render: (text: any, record: any) => {
+        if (text === null) {
+          return <span style={{ whiteSpace: 'pre-wrap' }}>none</span>;
+        }
+        if (text.status === 'error') {
+          containsAbnormal = true;
+          return (
+            <div style={{ display: 'flex' }}>
+              <span style={{ color: 'red', marginRight: '8px', whiteSpace: 'pre-wrap' }}>
+                {(text as IAbnormalValue).value}
+              </span>
+              {(text as IAbnormalValue).message && (
+                <Tooltip title={(text as IAbnormalValue).message}>
+                  <Icon style={{ paddingTop: '2px' }} type="question-circle" />
+                </Tooltip>
+              )}
+            </div>
+          );
+        }
+        if (text.status === 'warning' || text.status === 'info') {
+          return <span style={{ whiteSpace: 'pre-wrap' }}>{text.value}</span>;
+        }
+        // convert the server time format to local time
+        if (key === 'time' || key.endsWith('_time')) {
+          text = formatDatetime(text);
+        }
+        if (lessMoreColumns.includes(key)) {
+          return (
+            <div className="config_table_cell" id={`config_table_cell_${record.key}`}>
+              <div className="config_table_cell_summary">
+                <a onClick={(e: any) => toggleExpand(record, true)}>more</a>
+                <br />
+                <span>{text.slice(0, 50)}...</span>
+              </div>
+              <div className="config_table_cell_detail">
+                <a onClick={(e: any) => toggleExpand(record, false)}>less</a>
+                <br />
+                <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>
+              </div>
+            </div>
+          );
+        }
+        return <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>;
+      },
+    }));
+    const dataArr = (res as IResConclusionWithData).data.map((item, index) => ({
+      ...item,
+      key: `${index}`,
+    }));
+    setTableColumns(columns);
+    setDataSource(dataArr);
+    setConclusion(res.conclusion);
+    setHasAbnormal(containsAbnormal);
+  }
+
+  function parseResAsObj(res: IResObj) {
+    const columns = [
+      {
+        title: '信息',
+        dataIndex: 'field',
+        key: 'field',
+        render: (text: any) => <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>,
+      },
+      {
+        title: '值',
+        dataIndex: 'value',
+        key: 'value',
+        render: (text: any) => <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>,
+      },
+    ];
+    const dataArr = Object.keys(res).map(key => ({
+      field: key,
+      // convert the server time format to local time
+      value: key === 'time' || key.endsWith('_time') ? formatDatetime(res[key]) : res[key],
+      key,
+    }));
+    setTableColumns(columns);
+    setDataSource(dataArr);
+  }
+
   useEffect(() => {
     async function fetchData() {
       const res: IResReportItem = await request(fullApiUrl);
       if (res !== undefined) {
         // exist res.data filed, and res.data is an array
         if (res.data) {
-          let containsAbnormal =
-            (res.conclusion as IConclusion[]).find(item => item.status === 'error') !== undefined;
-          const columns = Object.keys(res.data[0] || {}).map(key => ({
-            title: key,
-            dataIndex: key,
-            key,
-            render: (text: any, record: any) => {
-              if (text === null) {
-                return <span style={{ whiteSpace: 'pre-wrap' }}>none</span>;
-              }
-              if (text.status === 'error') {
-                containsAbnormal = true;
-                return (
-                  <div style={{ display: 'flex' }}>
-                    <span style={{ color: 'red', marginRight: '8px', whiteSpace: 'pre-wrap' }}>
-                      {(text as IAbnormalValue).value}
-                    </span>
-                    {(text as IAbnormalValue).message && (
-                      <Tooltip title={(text as IAbnormalValue).message}>
-                        <Icon style={{ paddingTop: '2px' }} type="question-circle" />
-                      </Tooltip>
-                    )}
-                  </div>
-                );
-              }
-              if (text.status === 'warning' || text.status === 'info') {
-                return <span style={{ whiteSpace: 'pre-wrap' }}>{text.value}</span>;
-              }
-              // convert the server time format to local time
-              if (key === 'time' || key.endsWith('_time')) {
-                text = formatDatetime(text);
-              }
-              if (lessMoreColumns.includes(key)) {
-                return (
-                  <div className="config_table_cell" id={`config_table_cell_${record.key}`}>
-                    <div className="config_table_cell_summary">
-                      <a onClick={(e: any) => toggleExpand(record, true)}>more</a>
-                      <br />
-                      <span>{text.slice(0, 50)}...</span>
-                    </div>
-                    <div className="config_table_cell_detail">
-                      <a onClick={(e: any) => toggleExpand(record, false)}>less</a>
-                      <br />
-                      <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>
-                    </div>
-                  </div>
-                );
-              }
-              return <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>;
-            },
-          }));
-          const dataArr = (res as IResConclusionWithData).data.map((item, index) => ({
-            ...item,
-            key: `${index}`,
-          }));
-          setTableColumns(columns);
-          setDataSource(dataArr);
-          setConclusion(res.conclusion);
-          setHasAbnormal(containsAbnormal);
+          parseResAsArr(res as IResConclusionWithData);
         } else if (dataType === 'obj') {
           // res.data doesn't exist
           // res is an object
-          const columns = [
-            {
-              title: '信息',
-              dataIndex: 'field',
-              key: 'field',
-              render: (text: any) => <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>,
-            },
-            {
-              title: '值',
-              dataIndex: 'value',
-              key: 'value',
-              render: (text: any) => <span style={{ whiteSpace: 'pre-wrap' }}>{text}</span>,
-            },
-          ];
-          const dataArr = Object.keys(res).map(key => ({
-            field: key,
-            // convert the server time format to local time
-            value: key === 'time' || key.endsWith('_time') ? formatDatetime(res[key]) : res[key],
-            key,
-          }));
-          setTableColumns(columns);
-          setDataSource(dataArr);
+          parseResAsObj(res as IResObj);
         }
       }
     }
