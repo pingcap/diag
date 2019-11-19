863d4694a71d26bb590b716cbfb8036088ec6d6d
baurine <2008.hbl@gmail.com>
2019-11-19 11:05:16 +0800

    test  - add more test and comment for value formats

--- web/my-lib/value-formats/src/valueFormats/valueFormats.ts
+++ web/my-lib/value-formats/src/valueFormats/valueFormats.ts
@@ -28,6 +28,9 @@ let categories: ValueFormatCategory[] = [];
 const index: ValueFormatterIndex = {};
 let hasBuiltIndex = false;
 
+/////////////////////////////////////////////////////////////////
+
+// 固定小数位数
 export function toFixed(value: number, decimals?: DecimalCount): string {
   if (value === null) {
     return '';
@@ -36,10 +39,18 @@ export function toFixed(value: number, decimals?: DecimalCount): string {
     return value.toLocaleString();
   }
 
+  // 如果 decimals 为空，则 factor (系数因子?) 为 1 (即 10^0 = 1)
   const factor = decimals ? Math.pow(10, Math.max(0, decimals)) : 1;
+  // Math.round() 进行四舍五入
+  // 哦，这句话的作用就是用来抛弃掉多余的小数
+  // 比如 12.346，约定精确度为保留小数 2 位，目标值是 12.35
+  // 则先乘以 10^2 = 100，得到 1234.6，对它进行四舍五入，得到 1235，再重新除以 100，得到目标值 12.35
+  // 那就这已经得到目标值了啊，为什么不是直接返回呢？
+  // 考虑特殊情况 12.3，约定精确度为 2，预期值是 12.30，但到这一步只能得到 12.3
   const formatted = String(Math.round(value * factor) / factor);
 
   // if exponent return directly
+  // 什么时候会有指数出现? 估计是前面 String 的作用
   if (formatted.indexOf('e') !== -1 || value === 0) {
     return formatted;
   }
@@ -47,6 +58,8 @@ export function toFixed(value: number, decimals?: DecimalCount): string {
   // If tickDecimals was specified, ensure that we have exactly that
   // much precision; otherwise default to the value's own precision.
   if (decimals != null) {
+    // 补末尾的 0
+    // 12.3，保留 2 位小数，则得到 12.30，末尾要补一个 0
     const decimalPos = formatted.indexOf('.');
     const precision = decimalPos === -1 ? 0 : formatted.length - decimalPos - 1;
     if (precision < decimals) {
@@ -87,6 +100,7 @@ export function toFixedUnit(unit: string): ValueFormatter {
 // Formatter which scales the unit string geometrically according to the given
 // numeric factor. Repeatedly scales the value down by the factor until it is
 // less than the factor in magnitude, or the end of the array is reached.
+// factor 是比例系数，比如计算磁盘大小的 1024，计算时间的 60 (可是时间有 60 和 1000 两个 factor 耶...)
 export function scaledUnits(factor: number, extArray: string[]) {
   return (size: number, decimals?: DecimalCount, scaledDecimals?: DecimalCount) => {
     if (size === null) {
@@ -99,6 +113,7 @@ export function scaledUnits(factor: number, extArray: string[]) {
     let steps = 0;
     const limit = extArray.length;
 
+    // 循环计算直到 size 小于 factor，得到 steps，通过 steps 得到单位
     while (Math.abs(size) >= factor) {
       steps++;
       size /= factor;
@@ -108,8 +123,10 @@ export function scaledUnits(factor: number, extArray: string[]) {
       }
     }
 
+    // 如果 scaledDecimals 不为空，则以 scaledDecimals 为准
+    // 否则以 decimals 为准
     if (steps > 0 && scaledDecimals !== null && scaledDecimals !== undefined) {
-      decimals = scaledDecimals + 3 * steps;
+      decimals = scaledDecimals + 3 * steps; // 为什么是乘以 3，大概是以 factor = 1000 为准，1000 表示 3 个小数点
     }
 
     return toFixed(size, decimals) + extArray[steps];
@@ -134,6 +151,8 @@ export function simpleCountUnit(symbol: string) {
     return scaled + ' ' + symbol;
   };
 }
+
+/////////////////////////////////////////////////////////////////
 
 function buildFormats() {
   categories = getCategories();
