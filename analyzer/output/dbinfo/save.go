e31c98ec96289e4d856d0a669dcbb8a7791dddf7
mapleFU <1506118561@qq.com>
2019-12-02 16:29:02 +0800

    [Remove] Remove unused
    Signed-off-by: mapleFU <1506118561@qq.com>

--- analyzer/output/dbinfo/save.go
+++ analyzer/output/dbinfo/save.go
@@ -4,7 +4,6 @@ import (
 	"github.com/pingcap/tidb-foresight/analyzer/boot"
 	"github.com/pingcap/tidb-foresight/analyzer/input/dbinfo"
 	"github.com/pingcap/tidb-foresight/model"
-	"github.com/pingcap/tidb-foresight/utils/debug_printer"
 	ti "github.com/pingcap/tidb-foresight/utils/tagd-value/int64"
 	log "github.com/sirupsen/logrus"
 )
@@ -27,14 +26,12 @@ func (t *saveDBInfoTask) Run(m *boot.Model, schemas *dbinfo.DBInfo, c *boot.Conf
 				idxnum.SetTag("status", "error")
 				idxnum.SetTag("message", "please add index for this table")
 			}
-			toInsert := &model.DBInfo{
+			if err := m.InsertInspectionDBInfo(&model.DBInfo{
 				InspectionId: c.InspectionId,
 				DB:           schema.Name,
 				Table:        tb.Name.L,
 				Index:        idxnum,
-			}
-			log.Infof("saveDBInfoTask Insert %v", debug_printer.FormatJson(toInsert))
-			if err := m.InsertInspectionDBInfo(toInsert); err != nil {
+			}); err != nil {
 				log.Error("insert db info:", err)
 				return
 			}
