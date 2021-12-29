b78a368b7cef625c0b4f69ee19c62f4e03851f88
srstack <srstack0203@gmail.com>
2021-12-29 17:58:33 +0800

    init diag aduitlog

---
+++ cmd/diag/command/aduit.go
@@ -0,0 +1,41 @@
+// Copyright 2021 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package command
+
+import (
+	"github.com/pingcap/tiup/pkg/cluster/audit"
+	"github.com/pingcap/tiup/pkg/cluster/spec"
+	"github.com/spf13/cobra"
+)
+
+func newAuditCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "audit [audit-id]",
+		Short: "Show audit log of diag operation",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := spec.Initialize("diag"); err != nil {
+				return err
+			}
+			switch len(args) {
+			case 0:
+				return audit.ShowAuditList(spec.AuditDir())
+			case 1:
+				return audit.ShowAuditLog(spec.AuditDir(), args[0])
+			default:
+				return cmd.Help()
+			}
+		},
+	}
+	return cmd
+}
