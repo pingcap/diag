8f28c6e6666975cbe3213b15c888306bced290da
Allen Zhong <zhongbenli@pingcap.com>
2022-01-26 17:38:50 +0800

    add copyright headers

--- collector/insight/sysinfo/ntp.go
+++ collector/insight/sysinfo/ntp.go
@@ -1,7 +1,16 @@
-// Copyright © 2018 PingCAP Inc.
+// Copyright 2018 PingCAP, Inc.
 //
-// Use of this source code is governed by an MIT-style license that can be found in the LICENSE file.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Use ntpq to get basic info of NTPd on the system
 
 package sysinfo
