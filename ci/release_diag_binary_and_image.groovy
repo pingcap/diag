e8536dc1e4d8d01c9b7bcd5708bac898d8226484
nexustar <inexustar@gmail.com>
2023-09-08 19:38:48 +0800

    fix ci

--- ci/release_diag_binary_and_image.groovy
+++ ci/release_diag_binary_and_image.groovy
@@ -47,6 +47,7 @@ def run_with_pod(Closure body) {
             cloud: cloud,
             namespace: namespace,
             idleMinutes: 0,
+            nodeSelector: "kubernetes.io/arch=amd64",
             containers: [
                     containerTemplate(
                             name: 'golang', alwaysPullImage: false,
